da92d30cfe1649c19d3b45cfba4e2ab3cea095fa
Mukund Murali <mukund.m.murali@oracle.com>
2024-10-23 11:20:52 +0530

    Project upload

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+// Remove or comment out the import for the BookDemoToolbar component
+// import BookDemoToolbar from '../components/BookDemoToolbar';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <header>
+        {/* Your header content */}
+      </header>
+      
+      <main>
+        {/* Remove the following line or block */}
+        {/* <BookDemoToolbar /> */}
+        
+        {/* Rest of your main content */}
+      </main>
+      
+      <footer>
+        {/* Your footer content */}
+      </footer>
+    </div>
+  );
+};
+
+export default HomePage;
